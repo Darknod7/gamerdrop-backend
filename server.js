8b6ac0160ae85c675877b9778ee58f755352d109
Ismoilov-Nodir <nodismoilov@gmail.com>
2026-06-03 01:36:40 +0500

    fix static path and check gitignore

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 const path = require('path');
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
